c5619fc0388592612ee33ef3e2c390a0b191d87c
Sidney <sid@onlysid.com>
2023-08-10 00:27:41 +0100

    Fixing scrollto in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
       </div> */}
 
       <div className="absolute xs:bottom-10 bottom-32 w-full justify-center items-center">
-        <a className="flex justify-center z-40" href="#about">
+        <a className="flex justify-center z-40 relative" href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-white justify-center items-start p-2">
             <motion.div animate={{ y: [0, 24, 0] }} transition={{ duration: 1.5, repeat: Infinity, repeatType: 'loop' }} className="w-3 h-3 rounded-full bg-white mb-1" />
           </div>
