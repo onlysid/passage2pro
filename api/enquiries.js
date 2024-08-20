f6a3b522ee872f0bbfd05153d588ecfec820b878
Sidney <sid@onlysid.com>
2024-08-20 22:56:29 +0100

    Changing date of enquiries filter for admin area

--- api/enquiries.js
+++ api/enquiries.js
@@ -9,8 +9,8 @@ export default (req, res) => {
     });
 
     db.connect((err) => {
-        // Define a timestamp to get from (27th June 2024)
-        var timestamp = 1719442800;
+        // Define a timestamp to get from (9th August 2024)
+        var timestamp = 1723158000;
 
         if (err) {
             return res.status(500).json({ error: 'Error connecting to database' });
